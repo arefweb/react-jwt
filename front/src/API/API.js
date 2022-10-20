85efcdfac9b0e7fb0e643fa137c09b2a2b5ecb38
arefweb <arefmovahedz@gmail.com>
2022-10-20 12:05:38 +0330

    Root URL

--- front/src/API/API.js
+++ front/src/API/API.js
@@ -1,2 +1,6 @@
 
-export const URL = "https://jwt-login-logout.herokuapp.com";
+// export const URL = "https://jwt-login-logout.herokuapp.com";
+export const URL =
+  process.env.NODE_ENV === "production"
+    ? "https://jwt-login-logout.herokuapp.com"
+    : "http://localhost:4000";
